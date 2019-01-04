44e1b3f2d98e10b863d819fbeb41ff0a95378d93
Jason Wroe <jason_mark_wroe@hotmail.com>
2019-01-04 23:25:47 +0000

    Add the start of the chase sequence by adding MOV values and then modify based of the skill roll.

---
+++ CoCKeeperHelper/Controllers/ModifySpeedController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoCKeeperHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoCKeeperHelper.Controllers
+{
+    public class ModifySpeedController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Modify")]
+        public IActionResult Modify(ModifySpeedModel msm)
+        {
+
+            return View("");
+        }
+    }
+}
---
+++ CoCKeeperHelper/Controllers/SpeedController.cs
@@ -0,0 +1,32 @@
+﻿using CoCKeeperHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoCKeeperHelper.Controllers
+{
+    [Route("[controller]")]
+    public class SpeedController : Controller
+    {
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var cs = new ChaseSpeeds();
+
+            return View(cs);
+        }
+
+        [Route("Assign")]
+        public IActionResult Assign(ChaseSpeeds cs)
+        {
+            var mod = new ModifySpeedModel()
+            {
+                ChaserMov = cs.ChaserMov,
+                EscapeeMov = cs.EscapeeMov
+            };
+            return View("ModifySpeed", mod);
+        }
+    }
+}
